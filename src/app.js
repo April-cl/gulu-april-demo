78c8f68ee5bf79337fe08941321c6c044ad912a0
April <xcl8202@gmail.com>
2021-02-18 00:29:27 +0800

    使用 chai.expect 添加测试用例

--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
-import Vue from 'vue'
-import Button from './button'
-import Icon from './icon'
-import ButtonGroup from './button-group'
+import Vue from 'vue';
+import Button from './button';
+import Icon from './icon';
+import ButtonGroup from './button-group';
 
-Vue.component('g-button', Button)
-Vue.component('g-icon', Icon)
-Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-button', Button);
+Vue.component('g-icon', Icon);
+Vue.component('g-button-group', ButtonGroup);
 
 new Vue({
-    el: '#app',
-})
+  el: '#app',
+});
+
+import chai from 'chai';
+
+const expect = chai.expect;
+
+// 单元测试
+{
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  });
+  vm.$mount('#test');
+  let useElement = vm.$el.querySelector('use');
+  let href = useElement.getAttribute('xlink:href');
+  console.log(useElement);
+  expect(href).to.eq('#i-settings');
+  vm.$el.remove();
+  vm.$destroy();
+}
