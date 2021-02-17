76e189e981afff1a9bea3fb6a66fd4fb86977d9e
April <xcl8202@gmail.com>
2021-02-18 01:11:53 +0800

    添加3个测试用例

--- src/app.js
+++ src/app.js
@@ -36,3 +36,51 @@ const expect = chai.expect;
   vm.$el.remove();
   vm.$destroy();
 }
+{
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  });
+  vm.$mount();
+  let useElement = vm.$el.querySelector('use');
+  let href = useElement.getAttribute('xlink:href');
+  console.log(useElement);
+  expect(href).to.eq('#i-loading');
+  vm.$el.remove();
+  vm.$destroy();
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  });
+  vm.$mount(div);
+  let svg = vm.$el.querySelector('svg');
+  let {order} = window.getComputedStyle(svg);
+  expect(order).to.eq('2');
+  vm.$el.remove();
+  vm.$destroy();
+}
+{
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  });
+  vm.$mount();
+  vm.$on('click', function() {
+    console.log('click')
+    expect(1).to.eq(1)
+  })
+  let button = vm.$el;
+  button.click()
+}
