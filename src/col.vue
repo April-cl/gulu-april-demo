7af5beee4e9f7115d22a355bef5c422e23a7c450
April <xcl8202@gmail.com>
2021-03-05 00:42:06 +0800

    响应式基本完成

--- src/col.vue
+++ src/col.vue
@@ -49,16 +49,28 @@ export default {
       gutter: 0
     }
   },
+  methods: {
+    createClasses (obj, str='') {
+      if (!obj) {return []}
+      let array = []
+      if (obj.span) {
+        array.push(`col-${str}${obj.span}`)
+      }
+      if (obj.offset) {
+        array.push(`offset-${str}${obj.offset}`)
+      }
+    }
+  },
   computed: {
     colClass() {
       let {span, offset, ipad, narrowPc, pc, widePc} = this
+      let createClasses = this.createClasses
       return [
-        span && `col-${span}`,
-        offset && `offset-${offset}`,
-        ... (ipad && [`col-ipad-${ipad.span}`]),
-        ... (narrowPc && [`col-narrow-pc-${narrowPc.span}`]),
-        ... (pc && [`col-pc-${pc.span}`]),
-        ... (widePc && [`col-wide-pc-${widePc.span}`]),
+        ... createClasses({span, offset})
+        ... (ipad ? [`col-ipad-${ipad.span}`] : []),
+        ... (narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
+        ... (pc ? [`col-pc-${pc.span}`] : []),
+        ... (widePc ? [`col-wide-pc-${widePc.span}`] : []),
       ]
     },
     colStyle() {
@@ -85,7 +97,7 @@ export default {
       margin-left: ($n /24) * 100%;
     }
   }
-  @media (min-width: 577px) and (max-width: 768px) {
+  @media (min-width: 577px) {
     $class-prefix: col-ipad-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
@@ -99,7 +111,7 @@ export default {
       }
     }
   }
-  @media (min-width: 769px) and (max-width: 992px) {
+  @media (min-width: 769px) {
     $class-prefix: col-narrow-pc-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
@@ -113,7 +125,7 @@ export default {
       }
     }
   }
-  @media (min-width: 993px) and (max-width: 1200px) {
+  @media (min-width: 993px) {
     $class-prefix: col-pc-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
