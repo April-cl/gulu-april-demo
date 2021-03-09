20bbc1d4121dd14a6802eff1f83e69960e1119d2
April <xcl8202@gmail.com>
2021-03-10 01:00:13 +0800

    创建完成layout content sider header footer组件基本功能

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
-import Vue from 'vue';
-import Button from './button';
-import Icon from './icon';
-import ButtonGroup from './button-group';
-import Input from './input';
+import Vue from 'vue'
+import Button from './button'
+import Icon from './icon'
+import ButtonGroup from './button-group'
+import Input from './input'
 import Row from './row'
 import Col from './col'
+import Layout from './layout'
+import Content from './content'
+import Sider from './sider'
+import Header from './header'
+import Footer from './footer'
 
-Vue.component('g-button', Button);
-Vue.component('g-icon', Icon);
-Vue.component('g-button-group', ButtonGroup);
-Vue.component('g-input', Input);
+Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-input', Input)
 Vue.component('g-row', Row)
 Vue.component('g-col', Col)
+Vue.component('g-layout', Layout)
+Vue.component('g-content', Content)
+Vue.component('g-sider', Sider)
+Vue.component('g-header', Header)
+Vue.component('g-footer', Footer)
 
 new Vue({
   el: '#app',
