f1b1a00442cadc28083c715b3ed2647988e3828a
April <xcl8202@gmail.com>
2021-04-04 00:46:04 +0800

    添加single选项

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="collapseItem" @click="open=!open">
+  <div class="collapseItem" @click="toggle">
     <div class="title">
       {{title}}
     </div>
@@ -22,6 +22,27 @@ export default {
     return {
       open: false
     }
+  },
+  inject: ['eventBus'],
+  methods: {
+    toggle() {
+      if (this.open) {
+        this.open = false
+      } else {
+        this.open = true
+        this.eventBus && this.eventBus.$emit(':update:selected', this)
+      }
+    },
+    close() {
+      this.open = false
+    }
+  },
+  mounted() {
+    this.eventBus && this.eventBus.$on(':update:selected', (vm) => {
+      if (vm !== this) {
+        this.close()
+      }
+    })
   }
 }
 </script>
