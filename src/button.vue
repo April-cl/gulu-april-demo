db2780fa6883316473f3f2b86aec66876214c0c7
April <xcl8202@gmail.com>
2021-04-10 15:01:35 +0800

    创建官网

--- src/button.vue
+++ src/button.vue
@@ -2,7 +2,7 @@
   <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
     <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
     <g-icon class="icon loading" v-if="loading" name="loading" :loading="loading"></g-icon>
-    <div class="content">
+    <div class="g-button-content">
       <slot></slot>
     </div>
   </button>
@@ -31,27 +31,35 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+$font-size: 14px;
+$button-height: 32px;
+$button-bg: white;
+$button-active-bg: #eee;
+$border-radius: 4px;
+$color: #333;
+$border-color: #999;
+$border-color-hover: #666;
 .g-button {
-  font-size: var(--font-size);
-  height: var(--button-height);
+  font-size: $font-size;
+  height: $button-height;
   padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
+  border-radius: $border-radius;
+  border: 1px solid $border-color;
+  background: $button-bg;
   display: inline-flex;
   justify-content: center;
   align-items: center;
   vertical-align: top;
   &:hover {
-    border-color: var(--border-color-hover);
+    border-color: $border-color-hover;
   }
   &:active {
-    background-color: var(--button-active-bg);
+    background-color: $button-active-bg;
   }
   &:focus {
     outline: none;
   }
-  > .content {
+  > .g-button-content {
     order: 2;
   }
   > .icon {
@@ -63,7 +71,7 @@ export default {
   }
 
   &.icon-right {
-    > .content {
+    > .g-button-content {
       order: 1;
     }
     > .icon {
